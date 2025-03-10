9cc6761db7cdb4d6bd9338d7738ef68c24f57b4b
cihanklc <ghunklc@gmail.com>
2025-03-10 16:59:46 +0300

    Complete the test class for Roman

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -7,30 +7,44 @@ public class RomanNumeralTest {
 
 
     @Test
-    public void singleNumber() {
+    public void singleNumbero() {
         RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
-    public void numberWithManyDigits() {
+    public void numberWithManyDigitse() {
         RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
-    public void numberWithSubtractiveNotation() {
+    public void numberWithSubtractiveNotationf() {
         RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
-    public void numberWithAndWithoutSubtractiveNotation() {
+    public void numberWithAndWithoutSubtractiveNotationff() {
         RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
+    }
+
+    @Test
+    public void numberWithAndWithoutSubtractiveNotationn() {
+        RomanNumeral roman = new RomanNumeral();
+        int result = roman.convert("LXXXX");
+        Assertions.assertEquals(90, result);
+    }
+
+    @Test
+    public void numberWithAndWithoutSubtractiveNotationeg() {
+        RomanNumeral roman = new RomanNumeral();
+        int result = roman.convert("XIIX");
+        Assertions.assertEquals(18, result);
     }
 }
